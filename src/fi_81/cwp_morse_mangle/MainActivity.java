644a28807630cc290c07d54254651f3de9bad18c
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2012-04-13 19:42:23 +0300

    Corrected the formating of activity source file
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@mbnet.fi>

--- src/fi_81/cwp_morse_mangle/MainActivity.java
+++ src/fi_81/cwp_morse_mangle/MainActivity.java
@@ -6,17 +6,17 @@ import android.view.Menu;
 import android.view.MenuInflater;
 
 public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
+	/** Called when the activity is first created. */
 	@Override
-	public boolean onCreateOptionsMenu (Menu menu) {
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
 		MenuInflater inflater = getMenuInflater();
-	    inflater.inflate(R.menu.main_menu, menu);
-	    return true;
+		inflater.inflate(R.menu.main_menu, menu);
+		return true;
 	}
 }
