96ebf458639db00a0d1c0e59ac43d77f09312805
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2012-04-14 17:28:41 +0300

    Add Activity lifetime debug-logging
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@mbnet.fi>

--- src/fi_81/cwp_morse_mangle/MainActivity.java
+++ src/fi_81/cwp_morse_mangle/MainActivity.java
@@ -3,22 +3,71 @@ package fi_81.cwp_morse_mangle;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+	private static final String TAG = "MainActivity";
 	private static final int SETUP_RESULT = 0xbeef;
 	
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate()");
+		
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.main);
 	}
 
 	@Override
+	public void onRestart() {
+		Log.d(TAG, "onRestart()");
+
+		super.onRestart();
+	}
+
+	@Override
+	public void onStart() {
+		Log.d(TAG, "onStart()");
+
+		super.onStart();
+	}
+	
+	@Override
+	public void onResume() {
+		Log.d(TAG, "onResume()");
+
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		Log.d(TAG, "onPause()");
+		
+		super.onPause();
+	}
+	
+	@Override
+	public void onStop() {
+		Log.d(TAG, "onStop()");
+		
+		super.onStop();
+	}
+	
+	@Override
+	public void onDestroy() {
+		Log.d(TAG, "onDestroy()");
+		
+		super.onDestroy();
+	}
+
+	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		Log.d(TAG, "onCreateOptionsMenu()");
+		
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main_menu, menu);
 		return true;
@@ -26,6 +75,8 @@ public class MainActivity extends Activity {
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		Log.d(TAG, "onOptionsItemSelected()");
+		
 	    // Handle item selection
 	    switch (item.getItemId()) {
 	        case R.id.menu_settings:
--- src/fi_81/cwp_morse_mangle/MainSettingsActivity.java
+++ src/fi_81/cwp_morse_mangle/MainSettingsActivity.java
@@ -8,8 +8,11 @@ import android.preference.EditTextPreference;
 import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.util.Log;
 
 public class MainSettingsActivity extends PreferenceActivity {
+	private static final String TAG = "MainSettingsActivity";
+	
 	/* default setup values */
 	public static final String HOSTNAME_DEFAULT = "cwp.opimobi.com";
 	public static final String HOSTPORT_DEFAULT = "20000";
@@ -22,6 +25,8 @@ public class MainSettingsActivity extends PreferenceActivity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
+		Log.d(TAG, "onCreate()");
+		
 		super.onCreate(savedInstanceState);
 
 		/* load settings.xml for preferences layout */
@@ -55,5 +60,47 @@ public class MainSettingsActivity extends PreferenceActivity {
 		}
 		
 		morseSpeed.setSummary(entries[idx]);
+	}
+	
+	@Override
+	public void onRestart() {
+		Log.d(TAG, "onRestart()");
+
+		super.onRestart();
+	}
+
+	@Override
+	public void onStart() {
+		Log.d(TAG, "onStart()");
+
+		super.onStart();
+	}
+	
+	@Override
+	public void onResume() {
+		Log.d(TAG, "onResume()");
+
+		super.onResume();
+	}
+	
+	@Override
+	public void onPause() {
+		Log.d(TAG, "onPause()");
+		
+		super.onPause();
+	}
+	
+	@Override
+	public void onStop() {
+		Log.d(TAG, "onStop()");
+		
+		super.onStop();
+	}
+	
+	@Override
+	public void onDestroy() {
+		Log.d(TAG, "onDestroy()");
+		
+		super.onDestroy();
 	}
 }
