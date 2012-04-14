7c0a5948fa8b57619bfbb7ce56c1a018abdbee7f
Jussi Kivilinna <jussi.kivilinna@mbnet.fi>
2012-04-14 17:49:30 +0300

    Add SaveInstanceState/RestoreInstanceState logging
    Signed-off-by: Jussi Kivilinna <jussi.kivilinna@mbnet.fi>

--- src/fi_81/cwp_morse_mangle/MainActivity.java
+++ src/fi_81/cwp_morse_mangle/MainActivity.java
@@ -10,15 +10,14 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 	private static final String TAG = "MainActivity";
-	private static final int SETUP_RESULT = 0xbeef;
-	
+
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		Log.d(TAG, "onCreate()");
-		
+
 		super.onCreate(savedInstanceState);
-		
+
 		setContentView(R.layout.main);
 	}
 
@@ -35,56 +34,71 @@ public class MainActivity extends Activity {
 
 		super.onStart();
 	}
-	
+
 	@Override
 	public void onResume() {
 		Log.d(TAG, "onResume()");
 
 		super.onResume();
 	}
-	
+
 	@Override
 	public void onPause() {
 		Log.d(TAG, "onPause()");
-		
+
 		super.onPause();
 	}
-	
+
 	@Override
 	public void onStop() {
 		Log.d(TAG, "onStop()");
-		
+
 		super.onStop();
 	}
-	
+
 	@Override
 	public void onDestroy() {
 		Log.d(TAG, "onDestroy()");
-		
+
 		super.onDestroy();
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		Log.d(TAG, "onSaveInstanceState()");
+
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		Log.d(TAG, "onRestoreInstanceState()");
+
+		super.onRestoreInstanceState(savedInstanceState);
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		Log.d(TAG, "onCreateOptionsMenu()");
-		
+
 		MenuInflater inflater = getMenuInflater();
 		inflater.inflate(R.menu.main_menu, menu);
 		return true;
 	}
-	
+
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		Log.d(TAG, "onOptionsItemSelected()");
-		
-	    // Handle item selection
-	    switch (item.getItemId()) {
-	        case R.id.menu_settings:
-	        	Intent i = new Intent(this, MainSettingsActivity.class);
-	        	startActivity(i);
-	        	return true;
-	    }
-	    
-	    return false;
+
+		// Handle item selection
+		switch (item.getItemId()) {
+		case R.id.menu_settings:
+			Intent i = new Intent(this, MainSettingsActivity.class);
+			startActivity(i);
+			return true;
+		}
+
+		return false;
 	}
 }
